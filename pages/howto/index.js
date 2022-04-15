1640fc0345b5892885d9d89df203e2ba38703d9f
srtp <srtp.th@gmail.com>
2022-04-15 22:28:28 +0700

    update build

--- pages/howto/index.js
+++ pages/howto/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Howto from "../../components/componentsPages/howto";
+import Howto from "../../components/componentsPages/Howto";
 import Nav from "../../components/header/Nav";
 
 function Index() {
